1c53d8c1522bb58a459eb4ead448093693707603
exassasinx <tanghonghuihonghui@gmail.com>
2018-09-29 05:53:31 +0800

    Add files via upload

---
+++ AllyXBodyBlocker/Bootstrap.cs
@@ -0,0 +1,40 @@
+﻿namespace AllyXBodyBlocker
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+
+    using Ensage.SDK.Menu;
+    using Ensage.SDK.Service;
+    using Ensage.SDK.Service.Metadata;
+
+    using Modes;
+
+    [ExportPlugin("Ally X Body Blocker", StartupMode.Auto, "Exassasinx")]
+    internal class Bootstrap : Plugin
+    {
+        private MenuFactory factory;
+
+        [ImportMany]
+        private IEnumerable<IBodyBlockMode> modes;
+
+        protected override void OnActivate()
+        {
+            factory = MenuFactory.Create("Ally X Body Blocker", "xdbodyBlocker");
+
+            foreach (var mode in modes)
+            {
+                mode.Activate();
+            }
+        }
+
+        protected override void OnDeactivate()
+        {
+            foreach (var mode in modes)
+            {
+                mode.Dispose();
+            }
+
+            factory.Dispose();
+        }
+    }
+}
---
+++ AllyXBodyBlocker/Modes/AllyHeroBlocker/AllyHeroBlockerSettings.cs
@@ -0,0 +1,24 @@
+﻿namespace AllyXBodyBlocker.Modes.AllyHeroBlocker
+{
+    using Ensage.Common.Menu;
+    using Ensage.SDK.Menu;
+
+    internal class AllyHeroBlockerSettings
+    {
+        public AllyHeroBlockerSettings()
+        {
+            var subFactory = MenuFactory.Attach("xdbodyBlocker").Menu("Ally Hero blocker");
+            Key = subFactory.Item("Hotkey", new KeyBind('-'));
+            Key.Item.Tooltip = "Block ally hero with your hero  (Hold)";
+            BlockSensitivity = subFactory.Item("Block sensitivity", new Slider(150, 100, 200));
+            BlockSensitivity.Item.Tooltip = "Bigger value will result in smaller block, but with higher success rate";
+            CenterCamera = subFactory.Item("Center camera", true);
+        }
+
+        public MenuItem<Slider> BlockSensitivity { get; }
+
+        public MenuItem<bool> CenterCamera { get; }
+
+        public MenuItem<KeyBind> Key { get; }
+    }
+}
